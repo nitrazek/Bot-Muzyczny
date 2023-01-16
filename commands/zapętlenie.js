7d5b1b6aed3def390ab75f705d1aedb2d28b695b
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-01-16 23:20:02 +0100

    Create zapętlenie.js

---
+++ commands/zapętlenie.js
@@ -0,0 +1,13 @@
+const { Client, CommandInteraction } = require('discord.js');
+
+module.exports = {
+    name: 'zapętlenie',
+    description: 'Zapętla obecną kolejkę',
+    /**
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+    async run(client, interaction) {
+
+    }
+}
